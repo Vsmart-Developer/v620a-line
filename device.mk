c743906fd70ff0a58fc66562f963fbbf5a2efcc4
thinhx2 <nthinh1102@gmail.com>
2020-06-10 13:12:33 +0700

    FOD: Adapt to our FOD impl

--- device.mk
+++ device.mk
@@ -15,10 +15,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 TARGET_SCREEN_HEIGHT := 2232
 TARGET_SCREEN_WIDTH := 1080
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    vendor/aicp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.V620A
